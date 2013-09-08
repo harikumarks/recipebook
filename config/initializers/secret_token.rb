b50d8a61691a9e81da70a96b0d8b3dc25dd9edb3
Harikumar Kumar Sulochana <harikumar.kumar.sulochana@sv.cmu.edu>
2013-09-07 19:47:10 -0700

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Recipebook::Application.config.secret_token = '52172d5786131be5b7d062547f5b0338f551e8303b0baabca7e23985217709237d1db39760c8964d9d7567599b98533696a40c74805f81cf60e004025af35016'
