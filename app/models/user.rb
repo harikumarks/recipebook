dd76ebc3f921155b36b43d090cd76e420280a69d
Harikumar Kumar Sulochana <harikumar.kumar.sulochana@sv.cmu.edu>
2013-09-08 12:49:56 -0700

    updated user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,10 @@
 class User < ActiveRecord::Base
-  attr_accessible :email, :name, :password
+  attr_accessible :email, :name, :password , :password_confirmation
+  has_secure_password
+  validates :name, presence: true,  length: { maximum: 50}
+  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+  validates :email, presence: true, format: { with: VALID_EMAIL_REGEX } , uniqueness: {case_sensitive: false }
+  validates :password, presence: true, length: { maximum: 15 , minimum: 6}
+  validates :password_confirmation, presence: true
+  before_save { |user|  user.email = email.downcase }
 end
